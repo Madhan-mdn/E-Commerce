9e6c9c7f6c5aafa9e0f000010191a7fd0ef6292a
Madhan <madhanb2604@gmail.com>
2026-09-04 18:15:24 +0530

    Implemented Responsive Navbar with Hamburger Button

--- js/products.js
+++ js/products.js
@@ -144,18 +144,30 @@ function addToCart(product) {
 }
 
 function updateCartCount() {
-    let cartCount = document.getElementById("cart-count");
-
-    if (!cartCount) {
-        return;
-    }
+    let cartCounts = document.querySelectorAll(".cart-count");
 
     let totalQuantity = cart.reduce((total, item) => {
         return total + item.quantity;
     }, 0);
 
-    cartCount.textContent = totalQuantity;
+    cartCounts.forEach((cartCount) => {
+        cartCount.textContent = totalQuantity;
+    });
 }
 
+let menuBtn = document.getElementById("menuBtn");
+let mobileMenu = document.getElementById("mobileMenu");
+let closeMenuBtn = document.getElementById("closeMenuBtn");
+
+if (menuBtn && mobileMenu && closeMenuBtn) {
+    menuBtn.addEventListener("click", () => {
+        mobileMenu.classList.remove("right-[-280px]");
+        mobileMenu.classList.add("right-0");
+    });
+    closeMenuBtn.addEventListener("click", () => {
+        mobileMenu.classList.remove("right-0");
+        mobileMenu.classList.add("right-[-280px]");
+    });
+}
 getProducts();
 updateCartCount();
